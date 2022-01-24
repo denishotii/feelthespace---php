0a986062f510904bbc065e8b8cd6bdfd24d5b7f0
Denis Hoti <64154721+denishotii@users.noreply.github.com>
2022-01-24 10:10:45 +0100

    Add files via upload

---
+++ shop/db/db.php
@@ -0,0 +1,13 @@
+<?php
+// sql105.byetcluster.com
+
+// $server = "fdb28.awardspace.net";
+// $username = "3780154_ishop";
+// $password = "denis2005";
+// $dbname = "3780154_ishop";
+
+// $connect=mysqli_connect($server,$username,$password,$dbname);
+//         mysqli_select_db($connect,$dbname);
+
+$connect=mysqli_connect("localhost","root","","feelthespace_shop");
+      mysqli_select_db($connect,"feelthespace_shop");
